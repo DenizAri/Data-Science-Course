7b6884bdd9ec5d779fafcb25b87afbf4940eeb08
DenizAri <58032174+DenizAri@users.noreply.github.com>
2021-02-07 11:29:28 +0000

    Week 4 is not complete yet

--- Data Science WEEK 1/Week5/Mock.py
+++ Data Science WEEK 1/Week5/Mock.py
@@ -1,19 +1,5 @@
 import pandas as pd
-df = pd.read_csv("MOCK_DATA.csv")
-df = df.set_index('Keyword')
-
-#get list of destinations
-destlist = df['MOCK_DATA'].tolist()
-
-#turn list into set to make it easier to work with
-destination = set(destlist)
-
-
-city = ['NewYork','Tucson','Rome','Athen','Barcelona','Florance']
-
-#loop to create a csv for each destination
-for dest in destination:
-    tempdf = df[(df['MOCK_DATA'] == dest)]
-    tempdf.to_csv(dest + ".csv")
-    print(tempdf.head())
-    #print(dest)
+holiday = pd.read_csv('MOCK_DATA.csv')
+print(holiday)
+cities = holiday[['first_name', 'city']]
+print(cities)
